f3477c85db5c678ac07ede522ed9c9d107cf48f2
gititdoneAMAN <amandevbhardwaj@gmail.com>
2024-08-20 23:37:50 +0530

    Added the Hero component

--- apps/client/components/Hero.tsx
+++ apps/client/components/Hero.tsx
@@ -1,78 +1,19 @@
 import React from "react";
+import DisplayImgCard from "./DisplayImgCard";
 
 const Hero = () => {
   return (
-    <div className="md:px-5 md:pb-5 pt-[5.5rem] px-2 pb-2 ">
-      <div className="grid grid-cols-1 md:grid-cols-3 gap-2 md:gap-4">
-        <div className="grid md:gap-4 gap-2">
-          <div>
-            <img
-              className="h-auto max-w-full rounded-lg"
-              src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image.jpg"
-              alt=""
-            />
-          </div>
-          <div>
-            <img
-              className="h-auto max-w-full rounded-lg"
-              src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-1.jpg"
-              alt=""
-            />
-          </div>
-          <div>
-            <img
-              className="h-auto max-w-full rounded-lg"
-              src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-2.jpg"
-              alt=""
-            />
-          </div>
-        </div>
-        <div className="grid gap-4">
-          <div>
-            <img
-              className="h-auto max-w-full rounded-lg"
-              src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-3.jpg"
-              alt=""
-            />
-          </div>
-          <div>
-            <img
-              className="h-auto max-w-full rounded-lg"
-              src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-4.jpg"
-              alt=""
-            />
-          </div>
-          <div>
-            <img
-              className="h-auto max-w-full rounded-lg"
-              src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-5.jpg"
-              alt=""
-            />
-          </div>
-        </div>
-        <div className="grid gap-4">
-          <div>
-            <img
-              className="h-auto max-w-full rounded-lg"
-              src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-6.jpg"
-              alt=""
-            />
-          </div>
-          <div>
-            <img
-              className="h-auto max-w-full rounded-lg"
-              src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-7.jpg"
-              alt=""
-            />
-          </div>
-          <div>
-            <img
-              className="h-auto max-w-full rounded-lg"
-              src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-8.jpg"
-              alt=""
-            />
-          </div>
-        </div>
+    <div className="md:px-5 md:pb-5 pt-[5.5rem] px-2 pb-2 flex items-center justify-center">
+      <div className="columns-1 md:columns-3 gap-4 space-y-4">
+        <DisplayImgCard img="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image.jpg" />
+        <DisplayImgCard img="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-1.jpg" />
+        <DisplayImgCard img="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-2.jpg" />
+        <DisplayImgCard img="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-3.jpg" />
+        <DisplayImgCard img="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-4.jpg" />
+        <DisplayImgCard img="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-5.jpg" />
+        <DisplayImgCard img="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-6.jpg" />
+        <DisplayImgCard img="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-7.jpg" />
+        <DisplayImgCard img="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-8.jpg" />
       </div>
     </div>
   );
