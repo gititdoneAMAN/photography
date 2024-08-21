b60855ce75c8e5085fe535ae03909281235206b9
gititdoneAMAN <amandevbhardwaj@gmail.com>
2024-08-21 23:27:49 +0530

    Upgraded the DisplayImage card

--- apps/client/components/DisplayImgCard.tsx
+++ apps/client/components/DisplayImgCard.tsx
@@ -1,16 +1,19 @@
 "use client";
 import React from "react";
 import { motion } from "framer-motion";
+import { useRouter } from "next/navigation";
 
 const DisplayImgCard = ({ img }: { img: string }) => {
+  const router = useRouter();
   return (
-    <div>
+    <div className="relative">
       <motion.img
         whileHover={{
           scale: 1.015,
           transition: { duration: 0.5, ease: "easeInOut" },
         }}
-        className="h-auto max-w-full rounded-lg"
+        onClick={() => router.push(`/photoView`)}
+        className="h-auto max-w-full rounded-lg cursor-pointer"
         src={img}
         alt=""
       />
