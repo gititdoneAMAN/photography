17a77a197c4bfc78d721b0a76eea63b49377706d
gititdoneAMAN <amandevbhardwaj@gmail.com>
2024-08-22 23:53:43 +0530

    Added the herotext

---
+++ apps/client/components/HeroText.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const HeroText = () => {
+  return (
+    <div className=" pt-[5.5rem] md:pt-[8.5rem] text-center px-2 md:px-5 text-xl md:text-3xl font-['oswald'] font-light">
+      "Crafting <span className="font-medium underline">Unique</span> Stories
+      Through Every Picture We{" "}
+      <span className="font-medium underline">Capture</span>."
+    </div>
+  );
+};
+
+export default HeroText;
