bbc304fde5bfe6d795d422754e7d4e78acc9f986
gititdoneAMAN <amandevbhardwaj@gmail.com>
2024-08-24 23:46:19 +0530

    Added the gallery page

---
+++ apps/client/app/(pages)/gallery/page.tsx
@@ -0,0 +1,12 @@
+import Hero from "../../../components/Hero";
+import HeroText from "../../../components/HeroText";
+import Navbar from "../../../components/Navbar";
+
+export default function Page(): JSX.Element {
+  return (
+    <div className="w-full overflow-x-hidden">
+      <Hero />
+      <Hero />
+    </div>
+  );
+}
