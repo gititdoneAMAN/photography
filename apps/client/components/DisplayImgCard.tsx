20ad417c020a562c66447ae090506927773072b9
gititdoneAMAN <amandevbhardwaj@gmail.com>
2024-08-31 22:42:36 +0530

    Removed the redundancies

--- apps/client/components/DisplayImgCard.tsx
+++ apps/client/components/DisplayImgCard.tsx
@@ -2,22 +2,36 @@
 import React from "react";
 import { motion } from "framer-motion";
 import { useRouter } from "next/navigation";
+import Image from "next/image";
 
-const DisplayImgCard = ({ img }: { img: string }) => {
+const DisplayImgCard = ({
+  img,
+  setShowCarousel,
+}: {
+  img: string;
+  setShowCarousel: any;
+}) => {
   const router = useRouter();
   return (
-    <div className="relative">
-      <motion.img
-        whileHover={{
-          scale: 1.015,
-          transition: { duration: 0.5, ease: "easeInOut" },
-        }}
-        onClick={() => router.push(`/photoView`)}
-        className="h-auto max-w-full rounded-lg cursor-pointer"
+    <motion.div
+      whileHover={{
+        scale: 1.015,
+        transition: { duration: 0.5, ease: "easeInOut" },
+      }}
+      className="relative h-full w-full"
+    >
+      <Image
         src={img}
         alt=""
+        className="h-auto max-w-full rounded-lg cursor-pointer"
+        onClick={() => {
+          setShowCarousel(true);
+        }}
+        width={614}
+        height={614}
+        priority
       />
-    </div>
+    </motion.div>
   );
 };
 
