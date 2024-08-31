817f930e48e0f98b51e5c9ea59dc123c02603372
gititdoneAMAN <amandevbhardwaj@gmail.com>
2024-08-31 22:47:31 +0530

    Configured the resource url

--- apps/client/next.config.js
+++ apps/client/next.config.js
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   transpilePackages: ["@repo/ui"],
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "d29ixn4v7wn1nw.cloudfront.net",
+        port: "",
+        pathname: "/**",
+      },
+    ],
+  },
 };
