a9ca673c9161ed48cbe15378c56f709465195cc6
gititdoneAMAN <amandevbhardwaj@gmail.com>
2024-08-31 22:43:10 +0530

    Refactored the code and added the cloudfront connection

--- apps/client/components/Hero.tsx
+++ apps/client/components/Hero.tsx
@@ -1,22 +1,14 @@
-import React from "react";
-import DisplayImgCard from "./DisplayImgCard";
+import React, { Suspense } from "react";
+import HeroImageRenderer from "./HeroImageRenderer";
+const DisplayImgCard = React.lazy(() => import("./DisplayImgCard"));
 
-const Hero = () => {
-  return (
-    <div className="md:px-5 md:pb-5 pt-[5.5rem] px-2 pb-2 flex items-center justify-center">
-      <div className="columns-1 md:columns-3 gap-4 space-y-4">
-        <DisplayImgCard img="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image.jpg" />
-        <DisplayImgCard img="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-1.jpg" />
-        <DisplayImgCard img="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-2.jpg" />
-        <DisplayImgCard img="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-3.jpg" />
-        <DisplayImgCard img="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-4.jpg" />
-        <DisplayImgCard img="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-5.jpg" />
-        <DisplayImgCard img="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-6.jpg" />
-        <DisplayImgCard img="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-7.jpg" />
-        <DisplayImgCard img="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-8.jpg" />
-      </div>
-    </div>
-  );
+const Hero = async () => {
+  // const imageURL = await getImageURL("hero/10.jpeg");
+  // console.log(imageURL);
+  // const objectsList = await getObjects();
+  // // @ts-ignore
+  // console.log(objectsList.Contents[0]?.Key || "undefined");
+  return <HeroImageRenderer />;
 };
 
 export default Hero;
