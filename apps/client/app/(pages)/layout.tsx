bf70acc72f3d10dc39b1f402832ebdba2a5ff8fd
gititdoneAMAN <amandevbhardwaj@gmail.com>
2024-08-31 22:29:37 +0530

    Adjusted the page layouts

--- apps/client/app/(pages)/layout.tsx
+++ apps/client/app/(pages)/layout.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Navbar from "../../components/Navbar";
+import Footer from "../../components/Footer";
 
 const layout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div>
       <Navbar />
       {children}
+      <Footer />
     </div>
   );
 };
--- apps/client/app/(services)/layout.tsx
+++ apps/client/app/(services)/layout.tsx
@@ -7,6 +7,7 @@ const layout = ({ children }: { children: React.ReactNode }) => {
     <div>
       <Navbar />
       {children}
+      <Footer />
     </div>
   );
 };
