6460faaf4227d18ee1e3a46004f262b0c333eaff
gititdoneAMAN <amandevbhardwaj@gmail.com>
2024-08-31 22:41:25 +0530

    Removed the unecessary portions

--- apps/client/app/page.tsx
+++ apps/client/app/page.tsx
@@ -1,17 +1,21 @@
-import Image from "next/image";
-import { Card } from "@repo/ui/card";
-import { Code } from "@repo/ui/code";
-import styles from "./page.module.css";
-import { Button } from "@repo/ui/button";
 import Hero from "../components/Hero";
-// import LocomotiveScroll from "locomotive-scroll";
+import Navbar from "../components/Navbar";
+import HeroText from "../components/HeroText";
+import Footer from "../components/Footer";
+import Testimonial from "../components/Testimonial";
+import Message from "../components/Message";
+import Services from "../components/Services";
 
 export default function Page(): JSX.Element {
-  // const locomotiveScroll = new LocomotiveScroll();
-
   return (
-    <div className="text-3xl w-full">
+    <div className="w-full overflow-x-hidden">
+      <Navbar />
+      <HeroText />
       <Hero />
+      <Services />
+      <Testimonial />
+      <Message />
+      <Footer />
     </div>
   );
 }
